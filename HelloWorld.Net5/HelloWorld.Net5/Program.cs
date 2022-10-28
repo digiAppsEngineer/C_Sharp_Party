42a9552a5582dd127d2c9941aaa7620be1fe5a25
digiAppsEngineer <105436626+digiAppsEngineer@users.noreply.github.com>
2022-10-28 16:54:02 -0400

    Add files via upload

---
+++ HelloWorld.Net5/HelloWorld.Net5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld.Net5
+{
+    internal class Program
+    {
+        // Entry point of our program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Pamela!");
+            Console.Read();
+        }
+    }
+}
