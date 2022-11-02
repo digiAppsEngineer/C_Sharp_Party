c8a7fbd777b7414c880f524d18de03c283d43daf
digiAppsEngineer <105436626+digiAppsEngineer@users.noreply.github.com>
2022-11-02 12:10:22 -0400

    conversion types

---
+++ Convos/Convos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Conersions
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // explicit conversion
+
+            double myDouble = 13.37;
+            int myInt;
+
+            // cast double to int
+            myInt = (int)myDouble;
+            ;
+
+            // implicit conversion
+
+            int num = 123456789;
+            long bigNum = num;
+
+            float myFloat = 13.37F;
+            double myNewDouble = myFloat;
+
+            // type conversion
+
+            string myString = myDouble.ToString();
+            // converts 13.372 to "13.72"
+            string myFloatString = myFloat.ToString();
+            bool sunIsShining = false;
+
+            string myBoolString = sunIsShining.ToString();
+
+
+            Console.WriteLine(myInt);
+            Console.WriteLine(myFloatString);
+            Console.WriteLine(myBoolString);
+            Console.Read();
+
+        }
+    }
+}
